e0c5385030aad2f32e9c91a701435dcda97368eb
= <santoshgiri2345@gmail.com>
2024-06-22 21:27:37 +0545

    sangeet

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native'
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Santosh Giri and Liza Giri love</Text>
+			<Text>Sangeet</Text>
+
 			<StatusBar style="auto" />
 		</View>
 	)
