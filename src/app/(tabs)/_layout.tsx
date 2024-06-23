b7d6f53e8d243635e28e8e5b18ad65c96bd61eda
= <santoshgiri2345@gmail.com>
2024-06-23 14:47:38 +0545

    added tabs bg and blur effect

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,8 @@
 import { colors, fontSize } from '@/constants/tokens'
+import { FontAwesome } from '@expo/vector-icons'
+import { BlurView } from 'expo-blur'
 import { Tabs } from 'expo-router'
+import { StyleSheet } from 'react-native'
 
 const TabsNavigation = () => {
 	return (
@@ -11,9 +14,33 @@ const TabsNavigation = () => {
 					fontWeight: '500',
 				},
 				headerShown: false,
+				tabBarStyle: {
+					position: 'absolute',
+					borderTopLeftRadius: 20,
+					borderTopRightRadius: 20,
+					borderTopWidth: 0,
+					paddingTop: 8,
+				},
+				tabBarBackground: () => (
+					<BlurView
+						intensity={95}
+						style={{
+							...StyleSheet.absoluteFillObject,
+							overflow: 'hidden',
+							borderTopLeftRadius: 20,
+							borderTopRightRadius: 20,
+						}}
+					/>
+				),
 			}}
 		>
-			<Tabs.Screen name="favorites" />
+			<Tabs.Screen
+				name="favorites"
+				options={{
+					title: 'Favorites',
+					tabBarIcon: ({ color }) => <FontAwesome name="heart" size={24} color={color} />,
+				}}
+			/>
 			<Tabs.Screen name="playlists" />
 			<Tabs.Screen name="(songs)" />
 			<Tabs.Screen name="artists" />
