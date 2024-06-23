3cbe2facc81cfe37c9e934c04ee526c9cf600376
= <santoshgiri2345@gmail.com>
2024-06-23 12:17:09 +0545

    songs screen layout

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -1,9 +1,10 @@
+import { defaultStyles } from '@/styles'
 import { Stack } from 'expo-router'
 import { View } from 'react-native'
 
 const SongsScreenLayout = () => {
 	return (
-		<View style={styles.container}>
+		<View style={defaultStyles.container}>
 			<Stack>
 				<Stack.Screen name="index" options={{ headerTitle: 'Songs' }} />
 			</Stack>
