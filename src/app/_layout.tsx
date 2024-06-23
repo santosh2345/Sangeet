59a95b5b54ac1ca63c4bb11c594cb18617c5b03d
= <santoshgiri2345@gmail.com>
2024-06-23 12:03:02 +0545

    safe area provider and status bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-export default function App() {
+const App = () => {
 	return (
-		<View style={styles.container}>
-			<Text>Sangeet</Text>
+		<SafeAreaProvider>
+			<RootNavigation />
 
 			<StatusBar style="auto" />
-		</View>
+		</SafeAreaProvider>
 	)
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
+const RootNavigation = () => {}
+
+export default App
---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { colors, fontSize } from '@/constants/tokens'
+import { StyleSheet } from 'react-native'
+
+export const defaultStyles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: colors.background,
+	},
+	text: {
+		fontSize: fontSize.base,
+		color: colors.text,
+	},
+})
+
+
+export cont utilsStyles = StyleSheet.create({})
