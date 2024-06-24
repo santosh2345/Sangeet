e2c4a8f1f02bc10fbb9d6528aa7ed0f2d955a36c
= <santoshgiri2345@gmail.com>
2024-06-24 16:45:51 +0545

    layout

--- src/app/(tabs)/(songs)/_layout.tsx
+++ src/app/(tabs)/(songs)/_layout.tsx
@@ -11,7 +11,7 @@ const SongsScreenLayout = () => {
 					name="index"
 					options={{
 						...StackScreenWithSearchBar,
-						headerTitle: 'Songs',
+						headerTitle: 'Songss',
 					}}
 				/>
 			</Stack>
