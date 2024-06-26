cb758be885e0973e4e9d466801087ab707651d26
= <santoshgiri2345@gmail.com>
2024-06-26 21:45:58 +0545

    music screen cleaned

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,6 @@
 import { defaultStyles } from '@/styles'
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 
 export default function MusicScreen() {
-	return (
-		<View style={defaultStyles.container}>
-			<Text
-				style={{
-					fontSize: 20,
-					fontWeight: 'bold',
-					color: 'white',
-					marginTop: 20,
-				}}
-			>
-				Music Screen
-			</Text>
-			<Text
-				style={{
-					fontSize: 20,
-					fontWeight: 'bold',
-					color: 'white',
-					marginTop: 20,
-				}}
-			>
-				Music Screen
-			</Text>
-
-			{/* ... other Text components ... */}
-		</View>
-	)
+	return <View style={defaultStyles.container}>{/* ... other Text components ... */}</View>
 }
