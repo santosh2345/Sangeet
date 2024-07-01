3a34c6aa5a03baa1054c262d8a65be7aa95ac566
= <santoshgiri2345@gmail.com>
2024-07-01 17:34:34 +0545

    optimized tracklist and fetch files

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,68 +1,21 @@
 // src/screens/SongsScreen.tsx
 import { defaultStyles } from '@/styles'
-import { MusicFile } from '@/types/MusicFile'
-import { fetchMusicFiles } from '@/utils/FetchMusicFiles'
-import * as MediaLibrary from 'expo-media-library'
-import React, { useCallback, useEffect, useState } from 'react'
-import { FlatList, Text, View } from 'react-native'
+import React from 'react'
+import { Text, View } from 'react-native'
 
-const SongsScreen: React.FC = () => {
-	const [hasPermission, setHasPermission] = useState<boolean>(false)
-	const [musicFiles, setMusicFiles] = useState<MusicFile[]>([])
-
-	const loadMusicFiles = useCallback(async () => {
-		if (!hasPermission) return
-
-		try {
-			const result = await fetchMusicFiles()
-			setMusicFiles(result.musicFiles)
-		} catch (error) {
-			console.error('Error fetching music files:', error)
-		} finally {
-		}
-	}, [hasPermission])
-
-	useEffect(() => {
-		;(async () => {
-			const permission = await MediaLibrary.requestPermissionsAsync()
-			setHasPermission(permission.status === 'granted')
-		})()
-	}, [])
-
-	useEffect(() => {
-		if (hasPermission) {
-			loadMusicFiles()
-		}
-	}, [hasPermission, loadMusicFiles])
-
-	const renderItem = ({ item }: { item: MusicFile }) => (
-		<Text style={styles.fileName}>{item.filename}</Text>
-	)
-
-	const keyExtractor = (item: MusicFile) => item.id
-
-	if (!hasPermission) {
-		return (
-			<View style={defaultStyles.container}>
-				<Text style={styles.fileName}>Permission not granted</Text>
-			</View>
-		)
-	}
-
+const SongsScreen = () => {
 	return (
 		<View style={defaultStyles.container}>
-			<FlatList data={musicFiles} renderItem={renderItem} keyExtractor={keyExtractor} />
+			<Text
+				style={{
+					color: 'white',
+					marginTop: 100,
+				}}
+			>
+				Songs
+			</Text>
 		</View>
 	)
-}
-
-const styles = {
-	fileName: {
-		fontSize: 14,
-		fontWeight: 'bold' as const,
-		marginTop: 10,
-		color: 'white',
-	},
 }
 
 export default SongsScreen
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,7 +1,68 @@
-import { FlatList } from 'react-native'
+import { defaultStyles } from '@/styles'
+import { MusicFile } from '@/types/MusicFile'
+import { fetchMusicFiles } from '@/utils/FetchMusicFiles'
+import { useCallback, useEffect, useState } from 'react'
+import { FlatList, Text, View } from 'react-native'
+
+import * as MediaLibrary from 'expo-media-library'
 
 const TracksList = () => {
-	return <FlatList data={[]} renderItem={() => null} />
+	const [hasPermission, setHasPermission] = useState<boolean>(false)
+	const [musicFiles, setMusicFiles] = useState<MusicFile[]>([])
+
+	const loadMusicFiles = useCallback(async () => {
+		if (!hasPermission) return
+
+		try {
+			const result = await fetchMusicFiles()
+			setMusicFiles(result.musicFiles)
+		} catch (error) {
+			console.error('Error fetching music files:', error)
+		} finally {
+		}
+	}, [hasPermission])
+
+	useEffect(() => {
+		;(async () => {
+			const permission = await MediaLibrary.requestPermissionsAsync()
+			setHasPermission(permission.status === 'granted')
+		})()
+	}, [])
+
+	useEffect(() => {
+		if (hasPermission) {
+			loadMusicFiles()
+		}
+	}, [hasPermission, loadMusicFiles])
+
+	const renderItem = ({ item }: { item: MusicFile }) => (
+		<Text style={styles.fileName}>{item.filename}</Text>
+	)
+
+	const keyExtractor = (item: MusicFile) => item.id
+
+	if (!hasPermission) {
+		return (
+			<View style={defaultStyles.container}>
+				<Text style={styles.fileName}>Permission not granted</Text>
+			</View>
+		)
+	}
+
+	return (
+		<View style={defaultStyles.container}>
+			<FlatList data={musicFiles} renderItem={renderItem} keyExtractor={keyExtractor} />
+		</View>
+	)
+}
+
+const styles = {
+	fileName: {
+		fontSize: 14,
+		fontWeight: 'bold' as const,
+		marginTop: 10,
+		color: 'white',
+	},
 }
 
 export default TracksList
